cf71593627a3a0a2a84b787a83a1a0386901559d
CeSium <fedir.katushonok@gmail.com>
2024-07-06 16:55:38 +0000

    Added GPIO and nrf24 init code

--- src/nrf24_mod.c
+++ src/nrf24_mod.c
@@ -28,6 +28,37 @@ static void nrf24_rx_active_timer_handler(struct timer_list *timer){
     // TODO: implement rx active timer handler
 }
 
+static irqreturn_t nrf24_irq_handler(int irq, void *dev_id){
+    // TODO: implement irq handler
+    return IRQ_HANDLED;
+}
+
+static int nrf24_tx_task(void *data){
+    // TODO: implement tx task
+    
+    return 0;
+}
+
+static void nrf24_destroy_devices(struct nrf24_device_t *nrf24_dev){
+    struct nrf24_pipe_t *pipe, *tmp;
+
+    list_for_each_entry_safe(pipe, tmp, &(nrf24_dev->pipes), list){
+        list_del(&(pipe->list));
+        cdev_del(&(pipe->cdev));
+        device_destroy(nrf24_dev->dev.class, MINOR(pipe->devt));
+        ida_simple_remove(pipe_ida, pipe->id);
+        kfree(pipe);
+    }
+}
+
+static void nrf24_gpio_free(struct nrf24_device_t *nrf24_dev){
+    free_irq(nrf24_dev->nrf24_hal_dev.spi->irq, nrf24_dev);
+
+    if(!IS_ERR(nrf24_dev->nrf24_hal_dev.ce)){
+        gpiod_put(nrf24_dev->nrf24_hal_dev.ce);
+    }
+}
+
 static struct nrf24_device_t *nrf24_device_init(struct spi_device *spi, struct class *nrf24_class){
     int ret;
     struct nrf24_device_t *nrf24_dev;
@@ -85,6 +116,33 @@ exit:
     return nrf24_dev;
 }
 
+static int nrf24_gpio_init(struct nrf24_device_t *nrf24_dev){
+    int ret;
+
+    nrf24_dev->nrf24_hal_dev.ce = gpiod_get(&(nrf24_dev->nrf24_hal_dev.spi->dev), "ce", GPIOD_ASIS);
+
+    if(IS_ERR(nrf24_dev->nrf24_hal_dev.ce)){
+        dev_err(&(nrf24_dev->dev), "%s: failed to get ce gpio\n", __func__);
+        return PTR_ERR(nrf24_dev->nrf24_hal_dev.ce);
+    }
+
+    nrf24_ce_off(&(nrf24_dev->nrf24_hal_dev));
+
+    // IRQF_TRIGGER_NONE was set because the trigger is set in the device tree
+    ret = request_irq(nrf24_dev->nrf24_hal_dev.spi->irq, nrf24_irq_handler, IRQF_TRIGGER_NONE, dev_name(&(nrf24_dev->dev)), nrf24_dev);
+
+    if(ret < 0){
+        dev_err(&(nrf24_dev->dev), "%s: failed to request irq\n", __func__);
+        goto err_gpio_put;
+    }
+
+    return 0;
+
+err_gpio_put:
+    gpiod_put(nrf24_dev->nrf24_hal_dev.ce);
+    return ret;
+}
+
 int nrf24_mod_probe(struct spi_device *spi, dev_t *devt, struct class *nrf24_class, struct ida *pipe_ida_ptr, struct ida *dev_ida_ptr){
     int ret;
     struct nrf24_device_t *nrf24_dev;
@@ -103,9 +161,37 @@ int nrf24_mod_probe(struct spi_device *spi, dev_t *devt, struct class *nrf24_cla
         return ret;
     }
 
+    nrf24_dev = nrf24_device_init(spi, nrf24_class);
+    if(IS_ERR(nrf24_dev)){
+        dev_err(&(spi->dev), "%s: failed to init nrf24 device\n", __func__);
+        return PTR_ERR(nrf24_dev);
+    }
+
+    ret = nrf24_gpio_init(nrf24_dev);
+    if(ret < 0){
+        dev_err(&(spi->dev), "%s: failed to init nrf24 gpio\n", __func__);
+        goto err_device_unregister;
+    }
+
+    // TODO: Create pipes and initialize NRF24 for RX mode
+
+    nrf24_dev->tx_task_struct = kthread_run(nrf24_tx_task, nrf24_dev, "nrf24-%d_tx_task", nrf24_dev->id);
+
+    if(IS_ERR(nrf24_dev->tx_task_struct)){
+        dev_err(&(spi->dev), "%s: failed to create tx task\n", __func__);
+        goto err_devices_destroy;
+    }
     
-    
+    spi_set_drvdata(spi, nrf24_dev);
+
     return 0;
+
+err_devices_destroy:
+    nrf24_destroy_devices(nrf24_dev);
+    nrf24_gpio_free(nrf24_dev);
+err_device_unregister:
+    device_unregister(&(nrf24_dev->dev));
+    return ret;
 }
 
 void nrf24_mod_remove(struct spi_device *spi, struct class *nrf24_class){
