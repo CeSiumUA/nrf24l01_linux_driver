2bfae6618c350a481c6ae8cb0316c0100818b433
CeSium <fedir.katushonok@gmail.com>
2024-07-13 09:17:14 +0000

    Uncommented some code

--- src/nrf24_mod.c
+++ src/nrf24_mod.c
@@ -505,21 +505,21 @@ static int nrf24_hal_rx_mode_init(struct nrf24_device_t *nrf24_dev){
         dev_err(&(nrf24_dev->dev), "%s: failed to setup retransmission\n", __func__);
         return -EIO;
     }
-    //FIXME
-    // if(nrf24_dev->config.mode == NRF24_PM_TX){
-    //     status = nrf24_set_ptx_mode(&(nrf24_dev->nrf24_hal_dev));
-    //     if(status != HAL_OK){
-    //         dev_err(&(nrf24_dev->dev), "%s: failed to set ptx mode\n", __func__);
-    //         return -EIO;
-    //     }
-    // }
-    // else{
-    //     status = nrf24_set_prx_mode(&(nrf24_dev->nrf24_hal_dev));
-    //     if(status != HAL_OK){
-    //         dev_err(&(nrf24_dev->dev), "%s: failed to set prx mode\n", __func__);
-    //         return -EIO;
-    //     }
-    // }
+    
+    if(nrf24_dev->config.mode == NRF24_PM_TX){
+        status = nrf24_set_ptx_mode(&(nrf24_dev->nrf24_hal_dev));
+        if(status != HAL_OK){
+            dev_err(&(nrf24_dev->dev), "%s: failed to set ptx mode\n", __func__);
+            return -EIO;
+        }
+    }
+    else{
+        status = nrf24_set_prx_mode(&(nrf24_dev->nrf24_hal_dev));
+        if(status != HAL_OK){
+            dev_err(&(nrf24_dev->dev), "%s: failed to set prx mode\n", __func__);
+            return -EIO;
+        }
+    }
 
     status = nrf24_set_radio_output_power(&(nrf24_dev->nrf24_hal_dev), nrf24_dev->config.tx_power);
     if(status != HAL_OK){
